9ae1bce18452de02782971e77a723288a4305588
parazyd <41898282+github-actions[bot]@users.noreply.github.com>
2023-06-05 17:29:24 +0000

    Generate mdbook

--- development/darkfi/consensus/sidebar-items.js
+++ development/darkfi/consensus/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["block","clock","constants","lead_coin","lead_info","proto","rcpt","state","stx","task","tx","types","utils","validator","wallet"]};
+window.SIDEBAR_ITEMS = {"mod":["block","clock","constants","fees","lead_coin","lead_info","proto","rcpt","state","stx","task","tx","types","utils","validator","wallet"]};
